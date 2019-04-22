d512b8f7ce38ed3963ca0030206820d065641b2c
Jeppe Ekstrand Halkjaer Madsen <jehm@sund.ku.dk>
2019-04-22 21:34:00 +0200

    documentation and use of switch function

--- R/tauPar.R
+++ R/tauPar.R
@@ -1,35 +1,33 @@
+#' Get Kendall's tau from parameter or parameter from Kendall's tau
+#'
+#' @title Get Kendall's tau from parameter or parameter from Kendall's tau
+#' @param par Parameter for frailty model
+#' @param dist Frailty distribution
+#' @param output Return tau or parameter
+#' @param type Parameterization of frailty distribution
+#' @return Kendall's tau from parameter or parameter from Kendall's tau
+#' @export
+#' @author Jeppe E. H. Madsen <jeppe.ekstrand.halkjaer@gmail.com>
 tauPar <- function(par = 0, dist = "gamma", output = "tau", type = "alpha"){
-    if(dist == "gamma"){
-        if(output == "tau"){
-            return(par / (2 + par))
-        }
-        else if(output == "par"){
-            return(2*par / (1-par))
-        }
-        else{
-            stop("output has to be either 'tau' or 'par'")
-        }
+    if(!(dist %in% c("gamma","posstab","invgauss"))){
+        stop("dist has to be either 'gamma', 'posstab' or 'invgauss'")
     }
-    else if(dist == "posstab"){
-        if(type == "alpha"){
-            return(1 - par)
-        }
-        else if(type == "theta"){
-            if(output == "tau"){
-                return(1 - 1 / par)
-            }
-            else if(output == "par"){
-                return(1 / (1-par))
-            }
-            else{
-                stop("type has to be either 'alpha' or 'theta'")
-            }
-        }
-        else{
-            stop("output has to be either 'tau' or 'par'")
-        }
+    if(!(output %in% c("tau","par"))){
+        stop("output has to be either 'tau' or 'par'")
     }
-    else if(dist == "invgauss"){
+    if(!(type %in% c("alpha","theta"))){
+        stop("type has to be either 'alpha' or 'theta'")
+    }
+    if(type=="theta"&output=="tau"&dist!="gamma"){
+        par <- 1/par
+    }
+    switch(dist, gamma={
+        switch(output, tau={
+            par/(2+par)
+        },par={
+            2*par/(1-par)
+        })
+    },posstab=1-par,invgauss={
         laplace <- function(alpha){
             L <- function(s) exp(alpha - sqrt(alpha) * sqrt(2*s + alpha))
             LL <- function(s){
@@ -39,31 +37,10 @@ tauPar <- function(par = 0, dist = "gamma", output = "tau", type = "alpha"){
             f <- function(s) s*L(s)*LL(s)
             4 * integrate(f, 0, Inf)$value - 1
         }
-        if(output == "tau"){
-            if(type == "alpha"){
-                laplace(par)
-            }
-            else if(type == "theta"){
-                laplace(1/par)
-            }
-            else{
-                stop("type has to be either 'alpha' or 'theta'")
-            }
-        }
-        else if(output == "par"){
-            if(type == "alpha"){
-                uniroot(function(alpha) laplace(alpha) - par, interval = c(.0001, 100))$root
-            }
-            else if(type == "theta"){
-                1 / uniroot(function(alpha) laplace(alpha) - par, interval = c(.0001, 100))$root
-            }
-            else{
-                stop("type has to be either 'alpha' or 'theta'")
-            }
-        }
-        else{
-            stop("output has to be either 'tau' or 'par'")
-        }
-    }
-    else{"dist has to be either 'gamma', 'posstab' or 'invgauss'"}
+        switch(output, tau={
+            laplace(par)
+        },par={
+            uniroot(function(alpha) laplace(alpha) - par, interval = c(.0001, 100))$root
+        })
+    })
 }
